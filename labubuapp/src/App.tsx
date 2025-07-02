782f58dd4469d744be458f95b53be402e921c222
Crustbrns <crustbrns@gmail.com>
2025-07-02 17:21:57 +0200

    Added new func

--- labubuapp/src/App.tsx
+++ labubuapp/src/App.tsx
@@ -2,7 +2,8 @@ import { useEffect } from "react";
 import WebApp from "@twa-dev/sdk";
 import "./App.css";
 import Labubupicker from "./components/labubupicker";
-import { IoGift, IoArrowRedo } from "react-icons/io5";
+import { IoGift, IoArrowRedo, IoCheckmarkCircleSharp } from "react-icons/io5";
+import { HiOutlineSpeakerphone } from "react-icons/hi";
 
 function App() {
   useEffect(() => {
@@ -51,6 +52,22 @@ function App() {
       </div>
       {/* <p>user: {WebApp.initDataUnsafe.user?.first_name}</p> */}
       <Labubupicker />
+      <div className="mt-6 text-white flex flex-col items-start gap-2">
+          <div className="ml-4">Условия получения</div>
+        <div
+          className="flex gap-0 flex-col w-full rounded-xl p-3 text-white items-center justify-center"
+          style={{backgroundColor: "rgb(55, 36, 58)"}}
+        >
+          <div className="flex items-center justify-center gap-4">
+              <div className="p-3 rounded-full flex" style={{backgroundColor: '#5FC6FF'}}><HiOutlineSpeakerphone size={22}/></div>
+              <div className="flex flex-col items-start text-start gap-1">
+                <div className="text-xl">Подписаться</div>
+                <div className="opacity-50 text-sm/4.5">Подпишитесь на необходимые каналы или группы спонсоров</div>
+              </div>
+              <div className=" bg-white p-0 rounded-full"><IoCheckmarkCircleSharp size={26} transform="scale(1.3)" color="#30D259"/></div>
+          </div>
+        </div>
+      </div>
       {/* <button onClick={handleClick}>send and close</button> */}
     </div>
   );
